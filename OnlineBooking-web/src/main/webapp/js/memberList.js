b85f432224c9b073c2b6b3be15512414ee2262bd
Eiichi <Eiichi@tnkmatic-spro3>
2015-07-03 10:16:08 +0900

    w2uiに換装中（サンプル通り実装するも動作していない）

--- OnlineBooking-web/src/main/webapp/js/memberList.js
+++ OnlineBooking-web/src/main/webapp/js/memberList.js
@@ -26,34 +26,106 @@ var w2gridColumns = [
     {field: 'memberGroupKbnName', caption: 'メンバーグループ', size: '120px'}
 ];
 
+//var config = {
+//    layout: {
+//        name: 'layout',
+//        padding: 0,
+//        panels: [
+//            {type:'top',  content:'top',  size:700, resizable: false},
+//            {type:'left', content:'left', size:200, resizable: true, minSize: 120},
+//            {type:'main', content:'main', size:500, resizable: true}
+//        ]
+//    },
+//    sidebar: {
+//        name: 'sidebar',
+//        nodes: [
+//            {id: 'memberRegist', text: 'メンバ登録'},
+//            {id: 'memberList', text: 'メンバ参照'}
+//        ],
+//        onClick: function() {
+//
+//        }
+//    },
+//    grid: {
+//        name: w2gridName,
+//        header: w2gridHeader,
+//        columns: w2gridColumns,
+//        onSelect: w2gridOnSelect,
+//        onUnselect: w2gridOnUnselect
+//    }  
+//};
+
 var config = {
     layout: {
         name: 'layout',
         padding: 0,
         panels: [
-            {type:'top',  content:'top',  size:700, resizable: false},
-            {type:'left', content:'left', size:200, resizable: true, minSize: 120},
-            {type:'main', content:'main', size:500, resizable: true}
+            { type: 'left', size: 200, resizable: true, minSize: 120 },
+            { type: 'main', minSize: 550, overflow: 'hidden' }
         ]
     },
     sidebar: {
-        name: 'slidebar',
-        nodes: [
-            {id: 'memberRegist', text: 'メンバ登録'},
-            {id: 'memberList', text: 'メンバ参照'}
+        name: 'sidebar',
+        nodes: [ 
+            { id: 'general', text: 'General', group: true, expanded: true, nodes: [
+                { id: 'grid1', text: 'Grid 1', img: 'icon-page', selected: true },
+                { id: 'grid2', text: 'Grid 2', img: 'icon-page' },
+                { id: 'html', text: 'Some HTML', img: 'icon-page' }
+            ]}
         ],
-        onClick: function() {
-
+        onClick: function (event) {
+            switch (event.target) {
+                case 'grid1':
+                    w2ui.layout.content('main', w2ui.grid1);
+                    break;
+                case 'grid2':
+                    w2ui.layout.content('main', w2ui.grid2);
+                    break;
+                case 'html':
+                    w2ui.layout.content('main', '<div style="padding: 10px">Some HTML</div>');
+                    $(w2ui.layout.el('main'))
+                        .removeClass('w2ui-grid')
+                        .css({ 
+                            'border-left': '1px solid silver'
+                        });
+                    break;
+            }
         }
     },
-    grid: {
-        name: w2gridName,
-        header: w2gridHeader,
-        columns: w2gridColumns,
-        onSelect: w2gridOnSelect,
-        onUnselect: w2gridOnUnselect
-    }  
-};
+    grid1: { 
+        name: 'grid1',
+        columns: [
+            { field: 'fname', caption: 'First Name', size: '180px' },
+            { field: 'lname', caption: 'Last Name', size: '180px' },
+            { field: 'email', caption: 'Email', size: '40%' },
+            { field: 'sdate', caption: 'Start Date', size: '120px' },
+        ],
+        records: [
+            { recid: 1, fname: 'John', lname: 'Doe', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 2, fname: 'Stuart', lname: 'Motzart', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 3, fname: 'Jin', lname: 'Franson', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 4, fname: 'Susan', lname: 'Ottie', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 5, fname: 'Kelly', lname: 'Silver', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 6, fname: 'Francis', lname: 'Gatos', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 7, fname: 'Mark', lname: 'Welldo', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 8, fname: 'Thomas', lname: 'Bahh', email: 'jdoe@gmail.com', sdate: '4/3/2012' },
+            { recid: 9, fname: 'Sergei', lname: 'Rachmaninov', email: 'jdoe@gmail.com', sdate: '4/3/2012' }
+        ]
+    },
+    grid2: { 
+        name: 'grid2',
+        columns: [
+            { field: 'state', caption: 'State', size: '80px' },
+            { field: 'title', caption: 'Title', size: '100%' },
+            { field: 'priority', caption: 'Priority', size: '80px', attr: 'align="center"' }
+        ],
+        records: [
+            { recid: 1, state: 'Open', title: 'Short title for the record', priority: 2 },
+            { recid: 2, state: 'Open', title: 'Short title for the record', priority: 3 },
+            { recid: 3, state: 'Closed', title: 'Short title for the record', priority: 1 },
+        ]
+    }
+}
 
 var w2gridOnSelect = function(event) {
     $('#memberDetailReference').attr('disabled', false);
@@ -70,6 +142,16 @@ var w2gridObj = {
     onSelect: w2gridOnSelect,
     onUnselect: w2gridOnUnselect
 };
+
+function init() {
+    w2utils.locale('/src/locale/ja-jp.json');
+    // initialization
+    $('#main').w2layout(config.layout);
+    w2ui.layout.content('left', $().w2sidebar(config.sidebar));
+    w2ui.layout.content('main', $().w2grid(config.grid1));
+    // in memory initialization
+    $().w2grid(config.grid2);
+}
 
 function memberReference() {
     var button = $(this);
--- OnlineBooking-web/src/main/webapp/jsp/mng/memberList.jsp
+++ OnlineBooking-web/src/main/webapp/jsp/mng/memberList.jsp
@@ -10,34 +10,27 @@
 <html>
     <head id="pageHeader">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" 
-              href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
-        <link rel="stylesheet" type="text/css" href="http://w2ui.com/src/w2ui-1.4.min.css" />
+        <link rel="stylesheet" type="text/css" href="http://w2ui.com/src/w2ui-1.4.2.min.css" />
         <link rel="stylesheet" type="text/css" href="../../css/OnlineBokking.css" />
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://w2ui.com/src/w2ui-1.4.2.min.js"></script>
+        <script type="text/javascript" src="../../js/common/onlineBooking.js"></script>
+        <script type="text/javascript" src="../../js/common/validation.js"></script>
+        <script type="text/javascript" src="../../js/memberList.js"></script>
         <title>オンライン授業予約</title>
     </head>
     <body>
-        <div id="pagebody"></div>
+        <div id="main"></div>
         <div id="debug">
             <ul id="debugarea"></ul>
         </div>
+        <script>
+            $(function (){
+                init();
+            });
+        </script>
     </body>
     
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/i18n/jquery-ui-i18n.min.js"></script>
-    <script type="text/javascript" src="../../js/lib/jquery.blockUI.js"></script>
-    <script type="text/javascript" src="../../js/common/onlineBooking.js"></script>
-    <script type="text/javascript" src="../../js/common/validation.js"></script>
-    <script type="text/javascript" src="../../js/memberList.js"></script>
-    <script type="text/javascript" src="http://w2ui.com/src/w2ui-1.4.min.js"></script>
-    <script>
-        $(function () {
-            w2utils.locale('/src/locale/ja-jp.json');
-            // レイアウト初期設定            
-            $('#pagebody').w2layout(config.layout);
-            w2ui.layout.content('left', $().w2sidebar(config.sidebar));
-            w2ui.layout.content('main', $().w2grid(config.grid));
-        });
-    </script>
+
 </html>
