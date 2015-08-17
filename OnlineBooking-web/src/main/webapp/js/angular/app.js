47f3e58683c09f32d4fdd70df45bbed61d9d11fb
tnkmatic@outlook.jp <tnkmatic@outlook.jp@win-fx8120>
2015-08-18 01:29:18 +0900

    AngularJSへの換装中

--- OnlineBooking-web/src/main/webapp/js/angular/app.js
+++ OnlineBooking-web/src/main/webapp/js/angular/app.js
@@ -3,4 +3,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var OnlineBookingModule = angular.module('onlineBookingModule', []);
+var OnlineBookingModule = angular.module('onlineBookingModule', ['ngMessages']);
