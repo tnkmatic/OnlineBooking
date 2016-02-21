8a3fb68ff1f5121a3753e5d98dcf1e9ed000442b
tnkmatic <tnkmatic@win-fx8120>
2016-02-22 01:18:05 +0900

    検索条件の組み込み対応中
    CriteriaAPIによる結合先テーブルへのWhere句による条件検索の実装が完了
    他の検索条件の実装の確認が未確認

--- OnlineBooking-ejb/src/main/java/com/tnkmatic/onlinebooking/ejb/dao/custom/BookingMemberCustom.java
+++ OnlineBooking-ejb/src/main/java/com/tnkmatic/onlinebooking/ejb/dao/custom/BookingMemberCustom.java
@@ -9,6 +9,7 @@ import com.tnkmatic.onlinebooking.ejb.common.ConstantValue;
 import com.tnkmatic.onlinebooking.ejb.entity.BookingMember;
 import com.tnkmatic.onlinebooking.ejb.entity.BookingMember_;
 import com.tnkmatic.onlinebooking.ejb.entity.RMemberMemberGroup;
+import com.tnkmatic.onlinebooking.ejb.entity.RMemberMemberGroup_;
 import com.tnkmatic.onlinebooking.ejb.resource.request.member.reference.MemberReferenceCondition;
 import java.util.List;
 import javax.ejb.Stateless;
@@ -38,30 +39,33 @@ public class BookingMemberCustom {
         CriteriaBuilder builder = em.getCriteriaBuilder();
         CriteriaQuery<BookingMember> criteriaQuery = builder.createQuery(BookingMember.class);
         //JPQLでの別名取得
-        Root<BookingMember> bm = criteriaQuery.from(BookingMember.class);
-        SetJoin<BookingMember, RMemberMemberGroup> bmg = bm.join(BookingMember_.rMemberMemberGroup);
+        Root<BookingMember> root = criteriaQuery.from(BookingMember.class);
+        Join<BookingMember, RMemberMemberGroup> join = root.join(BookingMember_.rMemberMemberGroup);
         
         //JPQLの生成
-        criteriaQuery = criteriaQuery.select(bm);
+        criteriaQuery = criteriaQuery.select(root);
         if (memberCondition.getLoginId() != null) { //ログインID
             criteriaQuery = criteriaQuery.where(
-                    builder.equal(bm.get(ConstantValue.MEMBER_CONDITION_LOGIN_ID), 
+                    builder.equal(root.get(ConstantValue.MEMBER_CONDITION_LOGIN_ID), 
                             memberCondition.getLoginId()));
         }
         if (memberCondition.getFirstName() != null) {   //氏名(名)
             criteriaQuery = criteriaQuery.where(
-                    builder.equal(bm.get(ConstantValue.MEMBER_CONDITION_FIRST_NAME),
+                    builder.equal(root.get(ConstantValue.MEMBER_CONDITION_FIRST_NAME),
                             memberCondition.getFirstName()));
         }
         if (memberCondition.getLastName() != null) {    //氏名(姓)
             criteriaQuery = criteriaQuery.where(
-                    builder.equal(bm.get(ConstantValue.MEMBER_CONDITION_LAST_NAME),
+                    builder.equal(root.get(ConstantValue.MEMBER_CONDITION_LAST_NAME),
                             memberCondition.getLastName()));
         }
         if (memberCondition.getMemberGroupKbn() != null) {  //グループ区分(講師 or 生徒 or 管理者)
             criteriaQuery = criteriaQuery.where(
-                    builder.equal(bm.get("bm.rMemberMemberGroup.memberGroupKbn"),
-                            memberCondition.getMemberGroupKbn()));
+                    builder.equal(join.get(RMemberMemberGroup_.memberGroupKbn), 
+                            memberCondition.getMemberGroupKbn())
+            );
+                    
+                    
         }
 
         //JPQLの発行
