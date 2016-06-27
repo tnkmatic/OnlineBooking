395aaeb84db34cf11d7fe13ad262fac875caac71
tnkmatic <tnkmatic@win-fx8120>
2016-06-28 01:25:56 +0900

    講師の属性追加画面を実装中
    日付のFrom-Toの整合性チェック用のカスタムディレクティブを実装完了

--- OnlineBooking-web/src/main/webapp/js/angular/directive/validators.js
+++ OnlineBooking-web/src/main/webapp/js/angular/directive/validators.js
@@ -4,9 +4,45 @@
  * and open the template in the editor.
  */
 
+
 angular.module('onlineBookingModule')
-        .directive('cd-dateFromTo') 
-            
-};
-
-
+        .directive('cdLessThanDate',['$parse', function($parse) {
+            return {
+                require: 'ngModel',
+                link: function(scope, element, attrs, controller) {
+                    /*
+                     * param1：スコープオブジェクト
+                     * param2：ディレクティブが指定された要素
+                     * param3：ディレクティブが指定された要素の属性群
+                     * param4：ディレクティブに適用されたコントローラ
+                     */
+                    scope.$watch( function() {
+                       var target = $parse(attrs.cdLessThanDate)(scope);
+                       return !controller.$modelValue || target <= controller.$modelValue;
+                    }, function(currentValue) {
+                        controller.$setValidity('lessThanDate', currentValue);
+                    });
+                }
+            };
+        }])
+        .directive('parseDirective', ['$parse', function($parse) {
+            return {
+                template : 'x : {{x}}, y : {{y}}, literal : {{isLiteral}}, constant : {{isConstant}}',
+                scope: {},
+                link : function(scope) {
+                    scope.x = 123;
+                    // scope.x を取得する関数を$parse経由で生成
+                    var getter = $parse('x');
+                    // scope.y に scope.x の値を設定
+                    scope.y = getter(scope);
+                    // scope.xに値を設定する関数を取得
+                    var setter = getter.assign;
+                    setter(scope, 456);
+                    // $parseに渡した式がリテラルかどうかの確認
+                    scope.isLiteral = getter.literal;
+                    // $parseに渡した式が変更可能かどうかの確認
+                    scope.isConstant = getter.constant;
+                }
+            };
+        }]);
+    
